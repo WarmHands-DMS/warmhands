9a751db08b2863c68ec40b5f692d99521be99d44
NavinduV <navinduvirajitha2001@gmail.com>
2024-08-17 03:46:37 +0530

    dotenv error handled

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,13 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import cookieParser from 'cookie-parser';
+import dotenv from "dotenv";
 
 import authRoute from "./routes/auth.route.js";
 import testRoute from "./routes/test.route.js"
 import userRoute from "./routes/user.route.js"
 
+dotenv.config();
 const port = 8800;
 const app = express();
 
