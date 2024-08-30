3081064a0afaaa2cfca91086d344786c48c7b868
Navindu Virajitha <navinduvirajitha2001@gmail.com>
2024-08-31 03:46:01 +0530

    News Page completed

--- frontend/src/components/Map/Map.jsx
+++ frontend/src/components/Map/Map.jsx
@@ -8,7 +8,7 @@ export const Map = ({items, zoom, latitude, longitude}) => {
     
 
     return (
-        <MapContainer className='map' center={[latitude, longitude]} zoom={zoom} scrollWheelZoom={false}>
+        <MapContainer className='map' center={[latitude, longitude]} zoom={zoom} scrollWheelZoom={true}>
         <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
